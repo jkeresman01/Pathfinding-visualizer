7ad4f6776bf837ea5334d637b061833fd5d134b7
josip <josipkeresman@gmail.com>
2024-05-28 20:02:04 +0200

    Rename constans to be consistent

--- source/headers/Global.h
+++ source/headers/Global.h
@@ -22,12 +22,12 @@ namespace gc{
         constexpr float HEIGHT = 10.0f; 
 
         constexpr float ORIGIN_X = WIDTH / 2;
-        constexpr float  ORIGIN_Y = HEIGHT / 2;
+        constexpr float ORIGIN_Y = HEIGHT / 2;
 
         constexpr float START_POSITION_X = 45.0f; 
         constexpr float START_POSITION_Y = 100.0f; 
 
-        constexpr float BORDER_THICKNESS = 0.5f; 
+        constexpr float OUTLINE_THICKNESS = 0.5f; 
 
         enum TYPE{
             EMPTY = 0,
