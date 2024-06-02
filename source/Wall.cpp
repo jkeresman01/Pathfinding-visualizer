9018a7dccdc9c5f15f0d2e40fbf8d193eb3ef9e6
josip <josipkeresman@gmail.com>
2024-06-02 16:23:24 +0200

    Remove not used methods in wall class

--- source/Wall.cpp
+++ source/Wall.cpp
@@ -5,10 +5,13 @@ Wall::Wall(){
     this->m_Wall.setSize(sf::Vector2f(gc::wall::WIDTH, gc::wall::HEIGHT));
     this->m_Wall.setOrigin(gc::wall::ORIGIN_X, gc::wall::ORIGIN_Y);
     this->m_Wall.setPosition(gc::node::START_POSITION_X, gc::node::START_POSITION_Y);
-    this->m_Wall.setFillColor(sf::Color::Black);
+    this->m_Wall.setFillColor(sf::Color(51, 51, 255));
     setVisible(true);
 }
 
+void Wall::draw(){
+    this->m_Window->draw(m_Wall);
+}
 bool Wall::isVisible(){
     return this->m_IsVisible;
 }
@@ -23,20 +26,4 @@ void Wall::setPosition(float postion_x, float position_y){
 
 void Wall::setVisible(bool is_visible){
     this->m_IsVisible = is_visible;
-}
-
-void Wall::draw(){
-    this->m_Window->draw(m_Wall);
-}
-
-void Wall::move(float position_x, float position_y){
-    this->m_Wall.move(position_x, position_y);
-}
-
-void Wall::rotate(float angle){
-    this->m_Wall.rotate(angle);
-}
-
-void Wall::setOrigin(float origin_x, float origin_y){
-    this->m_Wall.setOrigin(origin_x, origin_y);
 }
