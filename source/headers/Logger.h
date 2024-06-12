080d4afc08f9697199892c2615068a362593fb35
josip <josipkeresman@gmail.com>
2024-06-12 19:05:09 +0200

    Add logger implementation

---
+++ source/headers/Logger.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#define LOG(severity, message) \
+    std::cerr << severity ": file: " << __FILE__ << ", line: " << __LINE__ << ", msg: " << message << "\n";
+
+#define LOG_DEBUG(message) LOG("[DEBUG]", message)
+#define LOG_INFO(message) LOG("[INFO]", message)
+#define LOG_WARN(message) LOG("[WARN]", message)
+#define LOG_ERROR(message) LOG("[ERROR]", message)
