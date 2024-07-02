1e4b694d5a328ca52b0e1c7c5c7873ec4762368f
josip <josipkeresman@gmail.com>
2024-07-02 17:17:58 +0200

    Reformat logger implementation

--- source/headers/Logger.h
+++ source/headers/Logger.h
@@ -4,9 +4,9 @@
 #include <string>
 
 #define LOG(severity, message) \
-    std::cerr << severity ": file: " << __FILE__ << ", line: " << __LINE__ << ", msg: " << message << "\n";
+    std::cerr << severity "file: " << __FILE__ << ", line: " << __LINE__ << ", msg: " << message << "\n";
 
-#define LOG_DEBUG(message) LOG("[DEBUG]", message)
-#define LOG_INFO(message) LOG("[INFO]", message)
-#define LOG_WARN(message) LOG("[WARN]", message)
-#define LOG_ERROR(message) LOG("[ERROR]", message)
+#define LOG_DEBUG(message) LOG("[DEBUG]: ", message)
+#define LOG_INFO(message)  LOG("[INFO]:  ", message)
+#define LOG_WARN(message)  LOG("[WARN]:  ", message)
+#define LOG_ERROR(message) LOG("[ERROR]: ", message)
